e2ab204b17e7047dcff84bb4f7f1f83399e691f9
Anush79 <66175237+Anush79@users.noreply.github.com>
2022-11-18 15:37:39 +0530

    bug fixed

--- script.js
+++ script.js
@@ -7,36 +7,42 @@ let attempts = document.querySelector(".attempts");
 var number = Math.floor(Math.random() * 100);
 console.log(number);
 
-var count=0;
+var count = 0;
+var win = false;
 
 function check() {
-count++;
+  count++;
   var guess = Number(input.value);
-  if (guess < 1 || guess > 100) {
-    
-    errormsg.innerText = "Please enter a number between 1 and 100";
-    const myTimeout = setTimeout(function (){errormsg.innerText = "";}, 5000);
-       
-    
-  } else if(guess===number){
-    output.innerText=`Yay! You made it in ${count} attempts only. You are a genius`;
 
-  }
-  else if (guess > number) {
-    if (guess - number <= 10) {
-      output.innerText = "You are close to the number, just need a little effort";
-      
-    } else {
-      output.innerText = "Your Guess is too high";
+  if (!win) {
+    if (guess < 1 || guess > 100) {
+      errormsg.innerText = "Please enter a number between 1 and 100";
+      const myTimeout = setTimeout(function () {
+        errormsg.innerText = "";
+      }, 5000);
+    } else if (guess === number) {
+      output.innerText = `Yay! You made it in ${count} attempts only. You are a genius`;
+      win = true;
+    } else if (guess > number) {
+      if (guess - number <= 10) {
+        output.innerText =
+          "You are close to the number, just need a little effort";
+      } else {
+        output.innerText = "Your Guess is too high";
+      }
+    } else if (guess < number) {
+      if (number - guess < 11) {
+        output.innerText =
+          "You are close to the number. just need to put some more effort";
+      } else {
+        output.innerText = "Your Guess is too low";
+      }
     }
-  } else if (guess < number) {
-    if (number - guess < 11) {
-      output.innerText = "You are close to the number. just need to put some more effort";
-    } else {
-      output.innerText = "Your Guess is too low";
-    }
+    attempts.innerHTML += `<li>Attempt No. ${count}👉🏼 <span class="guessed_number"> ${guess}</span></li>`;
+  } 
+  else {
+    location.reload();
   }
-  attempts.innerHTML+=`<li>Attempt No. ${count}👉🏼 <span class="guessed_number"> ${guess}</span></li>`;
 }
 
 btn.addEventListener("click", check);
